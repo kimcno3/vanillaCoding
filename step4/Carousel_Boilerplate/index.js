3204fda135edb2b9dfc5e348d7b8a7dadc3281cf
kimcno3 <77563468+kimcno3@users.noreply.github.com>
2021-12-20 17:58:00 +0900

    도트 클릭 이벤트 핸들러에 조건문 추가
    - 버튼 외 다른 영역 클릭시 에러 발생을 제어하기 위한 조건문
    - target의 태그명이 div라면 함수 종료

--- step4/Carousel_Boilerplate/index.js
+++ step4/Carousel_Boilerplate/index.js
@@ -3,25 +3,36 @@ var images = ["images/image-1.png", "images/image-2.png", "images/image-3.png", 
 var btnLeft = document.querySelector(".button-left");
 var btnRight = document.querySelector(".button-right");
 var someImageElement = document.querySelector(".image");
-var imgIdx = 0;
+var dots = document.querySelector(".footer");
 
 // 최초 이미지
+var imgIdx = 0;
 someImageElement.src = images[imgIdx];
+
 // 우측버튼 클릭
 btnRight.addEventListener("click", function(ev){
     if(imgIdx === 4){
         imgIdx = 0;
-    }else {
+    } else {
         imgIdx++;
-    }
+    };
     someImageElement.src = images[imgIdx];
 });
 // 좌측버튼 클릭
 btnLeft.addEventListener("click", function(ev){
     if(imgIdx === 0){
         imgIdx = 4;
-    }else {
+    } else {
         imgIdx--;
-    }
+    };
     someImageElement.src = images[imgIdx];
+});
+// 도트 클릭
+dots.addEventListener("click", function(ev) {
+    if (ev.target.tagName==='DIV'){
+        return;
+    } else{
+        imgIdx = Number(ev.target.title)-1;
+        someImageElement.src = images[imgIdx];
+    };
 });
